09e90efe0d78d61ace6c886f834055b62e0afd26
shivam-vachhani <shivamv@systemintegration.in>
2026-06-12 19:01:55 +0530

    Backend: build pipeline inside the both ingest and query router

--- Backend/app/api/ingestRoute.py
+++ Backend/app/api/ingestRoute.py
@@ -1,16 +1,46 @@
-from fastapi import APIRouter
+import pathlib,shutil
+from fastapi import APIRouter,HTTPException
 from fastapi.responses import JSONResponse
 from app.models.ingest import IngestRequest
+from app.services.git_service import clone_repo
+from app.services.loader_service import get_code_files
+from app.services.chunking_service import chunk_files
+from app.services.vector_service import ingest_documents_to_chroma
+from app.services.rag_service import invalidate_cache
 
 router = APIRouter()
 
 @router.post('/ingest')
-def data_ingestion(req:IngestRequest):
-    
+async def data_ingestion(req:IngestRequest):
 
-    return JSONResponse(
-        status_code=200,
-        content={
-            "repo_url":str(req.repo_url)
-        }
-    )
+    repo_id,path = clone_repo(str(req.repo_url))
+
+    try:
+        if not repo_id:
+            raise HTTPException(status_code=400,detail="Clone repo failed please provaid valid url")
+        else:
+            print("Repo clonned....")
+        files = get_code_files(path)
+        print("Files loded....")
+        chunks = chunk_files(files)
+        print("Files chunked....") 
+        response = ingest_documents_to_chroma(chunks,repo_id)
+        print("Repo files stored....")
+        print(response)
+
+    finally:
+        if path and pathlib.Path(path).exists():
+            shutil.rmtree(path,ignore_errors=True)
+            print(f"[Ingest] Cleaned up clone at {path}")
+
+    if(response['status'] == "Success"):
+        invalidate_cache(repo_id)
+        return JSONResponse(
+            status_code=200,
+            content=response
+            )   
+    else:
+        raise HTTPException(
+            status_code=500,
+            detail="Some error occure in storing file in vector store"
+            )
--- Backend/app/api/queryRoute.py
+++ Backend/app/api/queryRoute.py
@@ -1,15 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from fastapi.responses import JSONResponse
 from app.models.query import QueryRequest
+from app.services.rag_service import get_rag_service,invalidate_cache
 router = APIRouter()
 
 @router.post('/query')
 def ask_query(req:QueryRequest):
-    return JSONResponse(
-        status_code=200,
-        content={
-            "query":req.question,
-            "repo_id":req.repo_id,
-        }
-    )
     
+    pipeline = get_rag_service(req.repo_id) 
+    response = pipeline.run(req.question)
+
+    if response:
+        return JSONResponse(
+            status_code=200,
+            content={
+            "query":req.question,
+            "response":response,
+            }
+         )
+    else:
+        raise HTTPException(status_code=400,detail="Can't genrate response")
